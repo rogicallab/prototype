898cf7e207181a9c44ecf14437e26acf62dd791f
tiyotiyo <tiyo.tiyo.yoti.yoti@gmail.com>
2020-03-16 13:52:58 +0900

    toolbarの追加

---
+++ Prototype4/Prototype3/app/src/main/java/com/example/prototype2/Room/Plan.java
@@ -0,0 +1,148 @@
+package com.example.prototype2.Room;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "plan_table")
+public class Plan {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "plan_name")
+    private String planName;
+
+    @ColumnInfo(name="category")
+    private String category;
+
+    @ColumnInfo(name="year")
+    private Integer year;
+
+    @ColumnInfo(name="month")
+    private Integer month;
+
+    @ColumnInfo(name="day")
+    private Integer day;
+
+    @ColumnInfo(name="hours")
+    private Integer hours;
+
+    @ColumnInfo(name="minute")
+    private Integer minute;
+
+    @ColumnInfo(name="notification")
+    private String notification;
+
+    @ColumnInfo(name="memo")
+    private String memo;
+    //コンストラクタ
+    public Plan(String planName,String category,Integer year,Integer month,Integer day,Integer hours,Integer minute,String notification,String memo){
+        this.planName=planName;
+        this.category=category;
+        this.year=year;
+        this.month=month;
+        this.day=day;
+        this.hours=hours;
+        this.minute=minute;
+        this.notification=notification;
+        this.memo=memo;
+    }
+
+
+
+
+
+    //Getter and Setter
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public Integer getMinute() {
+        return minute;
+    }
+
+    public String getNotification(){return notification;}
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPlanName(@NonNull String planName) {
+        this.planName = planName;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+    public void setMinute(Integer minute) {
+        this.minute = minute;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public void setAll(String planName,String category,Integer year,Integer month,Integer day,Integer hours,Integer minute,String notification,String memo){
+        this.planName=planName;
+        this.category=category;
+        this.year=year;
+        this.month=month;
+        this.day=day;
+        this.hours=hours;
+        this.minute=minute;
+        this.notification=notification;
+        this.memo=memo;
+    }
+}
---
+++ Prototype4/Prototype3/app/src/main/java/com/example/prototype2/ToDo/ToDoContentsFragment.java
@@ -0,0 +1,177 @@
+package com.example.prototype2.ToDo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.prototype2.Calendar.PlanListAdapter;
+import com.example.prototype2.R;
+import com.example.prototype2.Room.Plan;
+import com.example.prototype2.sharedViewModel;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ToDoContentsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ToDoContentsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private RecyclerView recyclerView;
+    private List<Plan> myDataset = new ArrayList<>();
+    final PlanListAdapter mAdapter=new PlanListAdapter(myDataset);
+    private RecyclerView.LayoutManager layoutManager;
+    private sharedViewModel mPlanViewModel;
+
+    public ToDoContentsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ToDoContentsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ToDoContentsFragment newInstance(String param1, String param2) {
+        ToDoContentsFragment fragment = new ToDoContentsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static ToDoContentsFragment newInstance(int page){
+        Bundle args = new Bundle();
+        args.putInt("page",page);
+        ToDoContentsFragment fragment = new ToDoContentsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        System.out.println("Contentsフラグメント作成");
+        // Inflate the layout for this fragment
+        int page = getArguments().getInt("page",0);
+        View view = inflater.inflate(R.layout.fragment_to_do_contents, container, false);
+
+        // 画面に表示する文字
+        //((TextView)view.findViewById(R.id.page_text)).setText("Page"+page);
+
+        //recycleView
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewT);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+//        mAdapter = new PlanListAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+        //クリックイベント
+        mAdapter.setOnItemClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Navigation.findNavController(view).navigate(R.id.action_toDoFragment_to_editPlan);
+                Navigation.findNavController(view).navigate(R.id.action_toDoFragment_to_editPlan);
+            }
+        });
+
+        //ViewModelと対応づける
+        mPlanViewModel = new ViewModelProvider(this).get(sharedViewModel.class);
+        SharedPreferences data = getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        System.out.println((String)gson.fromJson(data.getString("category",""), ArrayList.class).get(page-1));
+        mPlanViewModel.getByCategory((String)gson.fromJson(data.getString("category",""), ArrayList.class).get(page-1)).observe(getViewLifecycleOwner(), new Observer<List<Plan>>() {
+            @Override
+            public void onChanged(List<Plan> plans) {
+                mAdapter.setPlans(plans);
+            }
+        });
+
+
+        //スワイプなど
+        ItemTouchHelper mIth  = new ItemTouchHelper(
+                new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN ,
+                        ItemTouchHelper.LEFT) {
+
+                    @Override
+                    public boolean onMove(@NonNull RecyclerView recyclerView,
+                                          @NonNull RecyclerView.ViewHolder viewHolder,
+                                          @NonNull RecyclerView.ViewHolder target) {
+                        final int fromPos = viewHolder.getAdapterPosition();
+                        final int toPos = target.getAdapterPosition();
+                        mAdapter.notifyItemMoved(fromPos, toPos);
+                        return true;// true if moved, false otherwise
+                    }
+
+                    @Override
+                    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                        final int fromPos = viewHolder.getAdapterPosition();
+                        Plan myPlan=mAdapter.getPlanAtPosition(fromPos);
+                        try {
+                            mPlanViewModel.deletePlan(myPlan);
+                            myDataset.remove(fromPos);
+                            mAdapter.notifyItemRemoved(fromPos);
+
+                        }catch (IndexOutOfBoundsException e){
+                        }
+                    }
+                });
+        mIth.attachToRecyclerView(recyclerView);
+
+        return view;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
